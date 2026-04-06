78c957f9abfde9fe7bb5e348d3492f2af7f93ba4
Teddy-FN <teddy.amrullah@ist.id>
2026-04-06 16:50:59 +0700

    Adjust Next Config & Slicing UI Recipe Of The Day

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.themealdb.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- frontend/src/app/(main)/dashboard/page.jsx
+++ frontend/src/app/(main)/dashboard/page.jsx
@@ -1,8 +1,102 @@
 import React from "react";
+import {
+  getRecipeOfTheDay,
+  getAreas,
+  getCategories,
+} from "../../../../actions/mealdb.actions";
+import { ArrowRight, Flame, Globe } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const recipeData = await getRecipeOfTheDay();
+  const categoriesData = await getCategories();
+  const areasData = await getAreas();
+
+  const recipeOfTheDay = recipeData?.recipe || {};
+  const categories = categoriesData?.categories || [];
+  const areas = areasData?.areas || [];
   return (
-    <div className="min-h-screen bg-stone-50 py-16 px-4">DashboardPage</div>
+    <div className="min-h-screen bg-stone-50 py-16 px-4">
+      <div className="max-w-6xl mx-auto">
+        <div className="mb-5">
+          <h1 className="text-5xl md:text-7xl font-bold text-stone-900 mb-4 tracking-tight leading-tight">
+            Fresh Recipes, Servd Daily
+          </h1>
+          <p className="text-xl text-stone-600 font-light max-w-2xl">
+            Discover thousand of recipes from around the world. Cook, create,
+            and savor.
+          </p>
+        </div>
+
+        {/* Recipe Of The Day Section */}
+        {recipeOfTheDay && (
+          <section>
+            <div className="flex items-center gap-2 mb-6">
+              <Flame className="w-6 h-6 text-orange-600" />
+              <h2 className="text-3xl font-serif font-bold text-stone-600">
+                Recipe Of the day
+              </h2>
+            </div>
+
+            <Link
+              href={`/recipe?cook=${encodeURIComponent(recipeOfTheDay?.strMeal || "")}`}
+            >
+              <div className="relative bg-white border-2 border-stone-900 overflow-hidden hover:border-orange-600 hover:shadow-lg transition-all duration-300 group cursor-pointer">
+                <div className="grid md:grid-cols-2 gap-0">
+                  <div className="relative aspect-4/3 md:aspect-auto border-b-2 md:border-b-0 md:border-r-2 border-stone-900">
+                    <Image
+                      src={recipeOfTheDay.strMealThumb}
+                      alt={recipeOfTheDay.strMeal}
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
+
+                  <div className="p-8 md:p-12 flex flex-col justify-center">
+                    <div className="flex flex-wrap gap-2 mb-6">
+                      <Badge
+                        variant="outline"
+                        className="border-2 border-orange-600 text-orange-700 bg-orange-50 font-bold"
+                      >
+                        {recipeOfTheDay?.strCategory || "Unknown Category"}
+                      </Badge>
+                      <Badge
+                        variant="outline"
+                        className="border-2 border-stone-900 text-stone-700 bg-orange-50 font-bold"
+                      >
+                        <Globe className="w-3 h-3 mr-1" />
+                        {recipeOfTheDay?.strArea || "Unknown Cuisine"}
+                      </Badge>
+                    </div>
+
+                    <h3 className="text-4xl md:text-5xl font-bold text-stone-900 mb-4 group-hover:text-orange-600 transition-colors leading-tight">
+                      {recipeOfTheDay?.strMeal || "Unknown Recipe"}
+                    </h3>
+                    <p className="text-stone-600 mb-6 line-clamp-3 font-light text-lg">
+                      {`${recipeOfTheDay?.strInstructions?.substring(0, 200)}...` ||
+                        "No instructions available."}
+                    </p>
+
+                    <Button variant="primary" size="lg">
+                      Start Cooking <ArrowRight className="w-5 h-5 ml-2" />
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </section>
+        )}
+
+        {/* Browse By Categories */}
+        {categories && <section></section>}
+
+        {/* Browse By Cuisine */}
+        {areas && <section></section>}
+      </div>
+    </div>
   );
 };
 
